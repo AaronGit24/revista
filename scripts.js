664691476f6ce2533074b43ecd795775d9df0f54
AaronGit24 <152262204+AaronGit24@users.noreply.github.com>
2024-10-07 13:26:53 +0200

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,5 @@
+// scripts.js
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Document cargado y listo para usar.");
+    // Aquí puedes agregar más funcionalidades con JavaScript.
+});
